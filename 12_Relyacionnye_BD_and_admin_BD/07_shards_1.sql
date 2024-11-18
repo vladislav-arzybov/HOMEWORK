b0aeed848ca557f190d2dc934cec9cae24a9fc70
vladislav-arzybov <reivol@mail.ru>
2024-11-18 20:43:24 +0300

    Update 07_shards_1.sql

--- 12_Relyacionnye_BD_and_admin_BD/07_shards_1.sql
+++ 12_Relyacionnye_BD_and_admin_BD/07_shards_1.sql
@@ -1,1 +1,12 @@
+CREATE TABLE books
+(
+id bigint not null,
+category_id int not null,
+CONSTRAINT category_id_check CHECK (category_id <= 3),
+author character varying not null,
+title character varying not null,
+year int not null
+);
+CREATE INDEX books_category_id_idx ON books USING btree(category_id);
+
 
