01ee80f2519e913d2ad770c2c69ec223714da6fc
vladislav-arzybov <reivol@mail.ru>
2025-11-25 22:52:15 +0300

    Update create_vm.tf

--- 23_Diplom/02_k8s/02_infra/create_vm.tf
+++ 23_Diplom/02_k8s/02_infra/create_vm.tf
@@ -1,11 +1,11 @@
 ###Создание ВМ через цикл for_each
 
 resource "yandex_compute_instance" "k8s" {
-  for_each    = { for vm in var.each_vm : vm.vm_name => vm }
-  name        = each.value.vm_name
-  hostname    = each.value.vm_hostname
+  for_each    = var.each_vm
+  name        = each.value.name
+  hostname    = each.value.hostname
   platform_id = var.platform_id
-  zone        = var.zone-a
+  zone        = each.value.zone
     resources {
     cores         = each.value.cpu
     memory        = each.value.ram
@@ -22,7 +22,7 @@ resource "yandex_compute_instance" "k8s" {
     preemptible = var.vm_scheduling_policy_preemptible
   }
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-a.id
+    subnet_id = local.subnets[each.value.subnet]
     nat       = "true"
   }
 
@@ -33,45 +33,3 @@ resource "yandex_compute_instance" "k8s" {
   }
 
 }
-
-####################
-
-###Для удобства настройки ресурсов ВМ не стал переносить прееменные в variables.tf
-
-variable "each_vm" {
-  type = list(object({
-    vm_name     = string
-    vm_hostname = string
-    cpu         = number
-    ram         = number
-    disk_volume = number
-    fraction    = number
-  }))
-  default = [
-    {
-      vm_name     = "master-node-1"
-      vm_hostname = "master-node-1"
-      cpu         = 2
-      ram         = 2
-      disk_volume = 20
-      fraction    = 20
-    },
-    {
-      vm_name     = "worker-node-1"
-      vm_hostname = "worker-node-1"
-      cpu         = 2
-      ram         = 2
-      disk_volume = 20
-      fraction    = 20
-    },
-    {
-      vm_name     = "worker-node-2"
-      vm_hostname = "worker-node-2"
-      cpu         = 2
-      ram         = 2
-      disk_volume = 20
-      fraction    = 20
-    }
-  ]
-}
-
