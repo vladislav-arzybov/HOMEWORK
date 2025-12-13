b48b39f17f6198b5224152fcd455919d3f3938d1
vladislav-arzybov <reivol@mail.ru>
2025-12-13 12:48:18 +0300

    Update providers.tf

--- 23_Diplom/02_k8s/02_infra/providers.tf
+++ 23_Diplom/02_k8s/02_infra/providers.tf
@@ -8,7 +8,7 @@ terraform {
       version = "~> 2.4"
     }
   }
-  required_version = "~>1.8.4"
+  required_version = "~>1.11.3"
 }
 
 provider "yandex" {
@@ -17,4 +17,3 @@ provider "yandex" {
   zone      = var.zone-a
   service_account_key_file = file("~/.authorized_key.json")
 }
-
